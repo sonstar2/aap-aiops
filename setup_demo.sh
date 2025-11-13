9f990ef679bf7fa90d3567c7882210917761220f
Frederick Son <fredson@redhat.com>
2025-11-13 13:41:06 +1100

    Add close cr

---
+++ setup_demo.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+sandbox_num=
+demo_id=
+passwd=
+mattermost_token=
+gitea_token=
+snow_password=
+
+ansible-playbook setup_demo/setup_demo.yml -e "aap_hostname=controller.$demo_id.sandbox$sandbox_num.opentlc.com" -e "aap_username=lab-user" -e "aap_password=$passwd" -e "gitea_url=https://bastion.$demo_id.sandbox$sandbox_num.opentlc.com:488" -e "gitea_token=$gitea_token" -e "snow_instance=https://dev280409.service-now.com" -e "snow_username=admin" -e "snow_password=$snow_password" -e "mattermost_instance=http://service1.$demo_id.sandbox$sandbox_num.opentlc.com:8065" -e "mattermost_token=$mattermost_token"
+# ansible-playbook setup_demo/setup_demo.yml -e "aap_validate_certs=false" -e "aap_hostname=3.26.3.26" -e "aap_username=admin" -e "aap_password=R3dH4t123!" -e "gitea_url=https://bastion.$demo_id.sandbox$sandbox_num.opentlc.com:488" -e "gitea_token=$gitea_token" -e "snow_instance=https://dev342071.service-now.com" -e "snow_username=admin" -e "snow_password=$snow_password" -e "mattermost_instance=http://service1.$demo_id.sandbox$sandbox_num.opentlc.com:8065" -e "mattermost_token=$mattermost_token"
